1fa620454c9b769f96f54978b0414ec8ca3db989
Aleksandar Drakaliyski <theall3xander@gmail.com>
2024-01-28 14:27:06 +0000

    Notes can be deleted

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -16,6 +16,16 @@ const NotePage = () => {
       body: JSON.stringify(note)
     })
   }
+
+  let createNote = async () => {
+    fetch(`/api/notes/create/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note)
+    })
+  }
   
   let deleteNote = async () => {
     fetch(`/api/notes/${id}/delete/`, {
@@ -27,11 +37,18 @@ const NotePage = () => {
   }
   
   let handleSubmit = ()=> {
-    updateNote()
+    if (id !== 'new' && note.body==='') {
+      deleteNote()
+    } else if (id !== 'new') {
+      updateNote()
+    } else if (id==='new' && note.body !==null) {
+      createNote()
+    }
   }
   
   useEffect(() => {
     const getNote = async () => {
+      if (id==='new') return
       let response = await fetch(`/api/notes/${id}`);
       let data = await response.json();
       setNote(data);
@@ -44,11 +61,15 @@ const NotePage = () => {
       <div className="note-header">
         <h3>
           <Link to={'/'}>
-            < ArrowLeft onClick={handleSubmit()}/>
+            < ArrowLeft onClick={handleSubmit}/>
           </Link>
         </h3>
-        <Link to={'/'}>
-          <button onClick={deleteNote}>Delete</button>
+        <Link to={'/'}> 
+          {id !== 'new' ? (
+            <button onClick={deleteNote}>Delete</button>
+          ): (
+            <button onClick={handleSubmit}>Done</button>
+          )}
         </Link>
       </div>
       <textarea onChange={(e) =>{setNote({...note, 'body':e.target.value })}} defaultValue={note?.body}></textarea>
